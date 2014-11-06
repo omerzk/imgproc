a41c7bb50fe4263e172591406d27db4e9a04e4ad
omerzk <omeryom@gmail.com>
2014-11-06 13:38:20 +0200

    eq..in the works

--- ex1/histogramEqualize.m
+++ ex1/histogramEqualize.m
@@ -2,6 +2,16 @@ function [imEq, histOrig, histEq] = histogramEqualize(imOrig)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 if(imfinfo(imOrig).ColorType == 'truecolor')
-    channel = 
+    ImYIQ = transformRGB2YIQ(imOrig);%since i cant use a temp var
+    channel = im2uint8(ImYIQ(:, :, 1));
+else
+    channel = im2uint8(imOrig);  
 end
+histOrig = imhist(channel);
+dim = size(imOrig);
+LUT = round((cumsum(histOrig) * 255) / (dim(1) * dim(2)));
+
+    
+end
+
 
