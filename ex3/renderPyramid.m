bcc3e051d1476b45e99332d4b62c7f4d2ea8d36d
omerzk <mine!>
2014-12-09 11:37:31 +0200

    render py
    middle of changing render pyr

--- ex3/renderPyramid.m
+++ ex3/renderPyramid.m
@@ -1,11 +1,15 @@
 function res = renderPyramid(pyr, levels)
-    dims = size(pyr{1});
+    dim = size(pyr{1});
+    dims(1:levels) = dim(1);
+    sizeB = sum(dims./(2.^(1:levels)));
+    res = cell(1,sizeB);
     reqPyr = pyr(1:levels);
-    res = pyr{1};
+    res(1:dim(1),1:dim(2)) = pyr{1};
+    start = dim(1);
     cellfun(@accumelate , reqPyr(2:end))
-
-    function accumelate(lvl)
-        curdim = size(lvl);
-        res = cat(2, res, padarray(lvl,dims(1) - curdim(1), 255, 'post'));
-    end
-end
+ 
+     function accumelate(lvl)
+         curdim = size(lvl);
+         res(1:curdim(1),start:start + curdim(2))
+     end
+ end
