c9f39b5f0063a983e0cec48b4549493bdb516b67
omerzk <mine!>
2015-01-06 19:25:37 +0200

    added correct coordinate in feature
    still need to correct in descriptor

--- ex4/findFeatures.m
+++ ex4/findFeatures.m
@@ -12,20 +12,27 @@ last = 0;
 i = 1;
 maxCorners = min([800 maxNum]);
 corners = zeros(maxNum, 2);
+[szX szY] = size(pyr{1});
 while (found < maxNum) && (i < length(pyr))
-    level_corners = spreadOutCorners(pyr{i}, 7, 7, maxCorners);
-%     level_corners = corner(pyr{i},maxCorners);
-    numOfPoints = length(level_corners);
-    corners(last + 1:last + numOfPoints, :) = level_corners * (2^ (i - 1)) ;
+    %find the levels corners and transfer coordinates to pyr{1} 
+    level_corners = ((spreadOutCorners(pyr{i}, 7, 7, maxCorners) - 1)* (2^ (i - 1))) + 1;
+    X = level_corners(1,:);Y = level_corners(2,:);
+    %filter invalid feature points - unsamplable in 3rd pyr level
+    validPts = arrayfun(@(x,y)(x>12 && y > 12 && x<szX - 12 && y< szY - 12), X, Y);
+    validCorners = level_corners(:, validPts);
+    %save res and update points found and iteration
+    numOfPoints = length(validCorners);
+    corners(last + 1:last + numOfPoints, :) = validCorners;
     last = last + numOfPoints;
     found = found + numOfPoints;
-    i = i+1;
+    i = i + 1;
 end
+%==========================================================================
 pos = corners(1 : found, :);
+%do i need to filter image edge case?
 imshow(pyr{1})
 figure
 plot(pos,'r.','MarkerSize',10)
-
 desc = sampleDescriptor(pyr{3}, pos, 1);
 end
 
--- ex4/sampleDescriptor.m
+++ ex4/sampleDescriptor.m
@@ -7,6 +7,41 @@ function desc = sampleDescriptor(im,pos,descRad)
 % desc ? A kxkxn 3?d matrix containing the ith descriptor
 % at desc(:,:,i). The per?descriptor dimensions kxk are related to the
 % descRad argument as follows k = 1+2?descRad.
-d
+k = 7;
+G = fspecial('gaussian', [7 7], 2);
+%ataining the grad direction at every point of the image.
+%soble is the default method
+%blur to smooth the gradient.
+[~, dir] = imgradient(conv2(im, G, 'same'), 'sobel');
+[len, ~] = size(pos);
+desc = zeros(k, k, len);
+pos = pos
+for l = 1: len
+    x = pos(l, 1);
+    y = pos(l, 2);
+    curDir = dir(x, y);
+    cosTheta = cos(curDir);
+    sinTheta = sin(curDir);
+    %calculate the rotation matrix
+    %no need for homogunos coordinates
+    rotMat = [cosTheta -sinTheta; sinTheta cosTheta];
+    %make an index matrix which represents a window afound the target pixel
+    patchY = meshgrid(1:(descRad * 2) + 1, 1: (descRad * 2) + 1) - 4;
+    patchInd(:, :, 2) = patchY + y;
+    patchInd(:, :, 1) = patchY' + x;
+    sz = size(im);
+    %fabricate the origin coordinates
+    [Y, X] = meshgrid(1:sz(1), 1:sz(2));
+    patch = zeros(7, 7);
+    for m = 1:7
+        for k = 1:7
+            %as far as i can tell this is forward mapping i don't see a way
+            %to use backward warping...
+            %FIND THE X',Y' after rotation.
+            rotInd = rotMat * [patchInd(m, k, 1); patchInd(m, k, 2)];
+            patch(m, k) = interp2(Y, X, im, rotInd(2), rotInd(1));
+        end
+    end
+    desc(l) = patch;
 end
 
