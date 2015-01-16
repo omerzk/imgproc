2e8f3d11b48de3f46c1570c792f9b0704afe7e17
omerzk <omeryom@gmail.com>
2015-01-16 15:14:44 +0200

    ex4 backup is functional the rest is midwork

---
+++ ex4/matlabgetMod.m
@@ -0,0 +1,23 @@
+function H = matlabgetMod(origin, dist)
+
+original  = rgb2gray(origin);
+distorted  = rgb2gray(dist);
+
+ptsOriginal  = detectSURFFeatures(original);
+ptsDistorted = detectSURFFeatures(distorted);
+
+[featuresOriginal,   validPtsOriginal] = extractFeatures(original,  ptsOriginal);
+[featuresDistorted, validPtsDistorted] = extractFeatures(distorted, ptsDistorted);
+
+index_pairs = matchFeatures(featuresOriginal, featuresDistorted);
+
+matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
+matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
+
+figure; showMatchedFeatures(original,distorted,matchedPtsOriginal,matchedPtsDistorted);
+
+title('Matched SURF points, including outliers');
+[tform,inlierPtsDistorted,inlierPtsOriginal] = estimateGeometricTransform(matchedPtsDistorted,matchedPtsOriginal,'similarity');
+figure; showMatchedFeatures(original,distorted,inlierPtsOriginal,inlierPtsDistorted);
+title('Matched inlier points');
+H = tform.T
